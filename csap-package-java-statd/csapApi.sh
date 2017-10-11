4d6066b359462935a626eb5e13699dbfa2776224
peterdnight <p.eternightingale@gmail.com>
2017-10-11 08:50:00 -0400

    Update default cluster

--- csap-package-java-statd/csapApi.sh
+++ csap-package-java-statd/csapApi.sh
@@ -24,10 +24,10 @@ function printIt() {
 
 
 
-function buildAdditionalPackages() { displayHeader "No Source Packages" }
+function buildAdditionalPackages() { displayHeader "No Source Packages" ; }
 
 
-function getAdditionalBinaryPackages() { displayHeader "No Binary Packages" }
+function getAdditionalBinaryPackages() { displayHeader "No Binary Packages" ; }
 
 
 
